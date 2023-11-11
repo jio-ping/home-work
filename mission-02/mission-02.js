bdc7863965965336eac25fdc1845dad0f9d2a1da
jio <pennicillium@daum.net>
2023-11-12 01:28:50 +0900

    이메일 자바스크립트 추가

--- mission-02/mission-02.js
+++ mission-02/mission-02.js
@@ -1,15 +1,16 @@
+// 이메일 유효 함수
 function validateEmail() {
   let emailInput = document.getElementById("email");
   let errorMsg = document.getElementById("error-message");
+  let invalidIcon = document.getElementById("email-invalid-icon");
   let emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   let validEmail = emailReg.test(emailInput.value);
 
-  console.log(emailInput.value);
-  console.log(validEmail);
-
   if (validEmail) {
     errorMsg.innerText = "";
+    invalidIcon.classList.add("hidden");
   } else {
+    invalidIcon.classList.remove("hidden");
     errorMsg.innerText =
       "이메일 입력 방법이 잘못되었습니다. (예: user@domain.io)";
   }
